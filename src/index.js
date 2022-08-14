b66a6f1b043ba55f9c52d82536767f5d4c9078a1
Sharkri <sharkdude955@gmail.com>
2022-08-13 22:00:19 -0500

    you can add projects a

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import {
   subDays,
   addDays,
   formatDistanceStrict,
+  set,
 } from "date-fns";
 
 const APP = (function () {
   const todos = getLocalStorageItem("todos");
+  const projects = getLocalStorageItem("projects");
 
   function setLocalStorageItem(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -18,11 +20,22 @@ const APP = (function () {
     return item ? item : [];
   }
 
-  function createTodo(title, description, dueDate, priority) {
-    return { title, description, dueDate, priority };
+  function createTodo(projectName, title, description, dueDate, priority) {
+    todos.push({ projectName, title, description, dueDate, priority });
+    setLocalStorageItem("todos", todos);
+    return { projectName, title, description, dueDate, priority };
   }
 
-  return { getLocalStorageItem };
+  function createProject(projectName) {
+    projects.push({ projectName });
+    setLocalStorageItem("projects", projects);
+    return { projectName };
+  }
+
+  const getTodos = () => todos;
+  const getProjects = () => projects;
+
+  return { createTodo, createProject, getTodos, getProjects };
 })();
 const DOM = (function () {
   // initial selected
@@ -30,26 +43,57 @@ const DOM = (function () {
   links[0].classList.toggle("active");
 
   links.forEach((link) =>
-    link.addEventListener("click", (e) => {
-      removeAllActiveClass();
+    link.addEventListener("click", () => {
+      removeAllActiveClass(links);
+      removeAllActiveClass(projects.childNodes);
+      // highlight clicked link
       link.classList.toggle("active");
     })
   );
 
-  function removeAllActiveClass() {
-    links.forEach((element) => element.classList.remove("active"));
+  function removeAllActiveClass(element) {
+    element.forEach((e) => e.classList.remove("active"));
+  }
+  // declare selector
+  const modal = document.querySelector(".modal");
+  const modalForm = document.querySelector(".modal-form");
+  const cancel = document.querySelector(".cancel");
+  const submit = document.querySelector(".submit");
+  const svgArrow = document.querySelector(".open-project > svg");
+  const projects = document.querySelector(".projects");
+  // Initial load for Local Storage
+  for (let project of APP.getProjects()) appendProject(project.projectName);
+
+  const projectTab = document.querySelector(".projectTab");
+
+  projectTab.addEventListener("click", (e) => {
+    // show projects
+    if (e.target.contains(svgArrow)) {
+      svgArrow.classList.toggle("rotated");
+      return;
+    }
+    modal.classList.toggle("open");
+    modalForm.reset();
+  });
+
+  function appendProject(projectName) {
+    let project = document.createElement("div");
+    project.classList.add("project");
+    project.appendChild(document.createTextNode(projectName));
+    projects.appendChild(project);
   }
 
-  const projects = document.querySelector(".projects");
-  const svgArrow = document.querySelector(".openProject > svg");
+  cancel.addEventListener("click", () => modal.classList.toggle("open"));
+  submit.addEventListener("click", () => {
+    let name = document.querySelector("#name");
+    if (!name.value) return;
+    modal.classList.toggle("open");
+    APP.createProject(name.value);
+    appendProject(name.value);
+  });
   projects.addEventListener("click", (e) => {
-    console.log(e.target.contains(svgArrow));
-    // If add new project button was clicked
-    if (!e.target.contains(svgArrow)) {
-      document.querySelector(".modal").classList.toggle("open");
-      document.querySelector(".formModal").classList.toggle("open");
-    }
-    svgArrow.classList.toggle("rotated");
-    // show projects
+    removeAllActiveClass(links);
+    removeAllActiveClass(projects.childNodes);
+    e.target.classList.add("active");
   });
 })();
