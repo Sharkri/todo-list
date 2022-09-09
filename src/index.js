eabf160c77cf423591f97dab1be6a4a3becbb8a5
Sharkri <sharkdude955@gmail.com>
2022-09-09 18:46:35 -0500

    add cog wheel for edit todo

--- src/index.js
+++ src/index.js
@@ -307,25 +307,30 @@ const DOM = (function () {
       return;
     }
     // Edit Todo
+    if (!e.target.closest(".edit-todo")) return;
+
     todoForm.reset();
     resetTitleInput();
     toggleModal(addTodoModal);
-    const header = document.querySelector(".add-todo-modal > .modal-header");
-    header.textContent = "Edit Todo";
     const title = document.querySelector("#todo-title");
     const dueDate = document.querySelector("#due-date");
     const description = document.querySelector("#description");
     const priority = document.querySelector("#priority");
+
     let todo = APP.getTodoById(todoId);
+
     title.value = todo.title;
     dueDate.value = todo.dueDate;
     description.value = todo.description;
+
     if (todo.priority == "High") setSelectedOption(priority, 2);
     else if (todo.priority == "Medium") setSelectedOption(priority, 1);
+
     const projects = APP.getProjects();
     const selectedIndex = APP.findIndex(projects, "id", todo.projectId);
     addProjectOptions(selectedIndex);
-    addTodoModal.classList.add("editing");
+    // true because editing todo instead of adding
+    updateTodoModalEditing(true);
     addTodoModal.setAttribute("todo-index-number", todoId);
   });
 
@@ -335,8 +340,8 @@ const DOM = (function () {
     toggleModal(addTodoModal);
     const selectedIndex = getActiveProjectIndex();
     addProjectOptions(selectedIndex);
-    const header = document.querySelector(".add-todo-modal > .modal-header");
-    header.textContent = "Add Todo";
+    // false because adding todo instead of editing
+    updateTodoModalEditing(false);
   });
 
   links.forEach((link) => {
@@ -357,8 +362,6 @@ const DOM = (function () {
     setActiveClass(e.target.closest(".project"));
     const index = getActiveProjectIndex();
     const project = APP.getProject(index);
-    console.log(project.id);
-    console.log(APP.getTodos());
     switchTab(project.name, project.todos);
   });
 
@@ -477,17 +480,26 @@ const DOM = (function () {
     document.querySelector(".todos").classList.toggle("sidebar-hidden");
   });
 
+  function updateTodoModalEditing(isEditing) {
+    const header = document.querySelector(".add-todo-modal > .modal-header");
+    header.textContent = isEditing ? "Edit Todo" : "Add Todo";
+    submitTodo.textContent = isEditing ? "Update Todo" : "Add Todo";
+    isEditing
+      ? addTodoModal.classList.add("editing")
+      : addTodoModal.classList.remove("editing");
+  }
+
   function editTodo(todoId, projectId, title, description, dueDate, priority) {
     let todo = APP.getTodoById(todoId);
     let project = APP.getProjectById(projectId);
-    // check if actually setting to another value
+    // check if todo value is not the same
     if (todo.title !== title) todo.setTodoProperty("title", title);
     if (todo.description !== description)
       todo.setTodoProperty("description", description || "");
     if (todo.dueDate !== dueDate) todo.setTodoProperty("dueDate", dueDate);
     if (todo.priority !== priority) todo.setTodoProperty("priority", priority);
 
-    // check if same projectid selected
+    // check if same project selected
     if (todo.projectId === projectId) return;
     todo.setTodoProperty("projectId", projectId);
 
@@ -663,11 +675,20 @@ const DOM = (function () {
     check.classList.add("checkmark");
     markComplete.appendChild(check);
 
+    const edit = document.createElement("button");
+    const editSVG = createSVG(
+      "M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z",
+      "currentColor"
+    );
+    edit.appendChild(editSVG);
+    edit.classList.add("edit-todo");
+
     todoContainer.classList.add("todo");
     todoInfo.classList.add("todo-info");
     todoContainer.appendChild(markComplete);
     todoInfo.prepend(todoTitle);
     todoContainer.appendChild(todoInfo);
+    todoContainer.appendChild(edit);
     return todoContainer;
   }
 
