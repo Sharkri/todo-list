ec2d44b347d786f9eb057730ee5264cb788fd4d8
Sharkri <sharkdude955@gmail.com>
2022-12-09 16:51:31 -0600

    Fix sign up google button not working

--- src/handleAuth.js
+++ src/handleAuth.js
@@ -20,7 +20,7 @@ const signUpEmailError = document.querySelector('.sign-up-page .email-invalid');
 // Sign in selectors
 const signInPage = document.querySelector('.sign-in-page');
 const signInButton = document.querySelector('.sign-in');
-const signInWithGoogleBtn = document.querySelector('.sign-in-with-google');
+const signInGoogleButtons = document.querySelectorAll('.sign-in-with-google');
 const goToSignInPage = document.querySelector('.go-to-sign-in-page');
 const signInEmailError = document.querySelector('.sign-in-page .email-invalid');
 const signInPassword = document.querySelector('#sign-in-password');
@@ -109,7 +109,9 @@ async function handleSignIn() {
 }
 signUpButton.addEventListener('click', handleSignUp);
 signInButton.addEventListener('click', handleSignIn);
-signInWithGoogleBtn.addEventListener('click', signInWithGoogle);
+signInGoogleButtons.forEach((btn) =>
+  btn.addEventListener('click', signInWithGoogle)
+);
 signOutButton.addEventListener('click', signOutUser);
 goToSignUpPage.addEventListener('click', goToSignUp);
 goToSignInPage.addEventListener('click', goToSignIn);
